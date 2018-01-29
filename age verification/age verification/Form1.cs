d8a122227e673e08d2decf77a06326b21a7362cd
John Brosius <brosius01@gmail.com>
2018-01-29 12:53:55 -0600

    Add files via upload

---
+++ age verification/age verification/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace age_verification
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            int ageInput;
+
+            ageInput = Convert.ToInt32(txtEnterAge.Text);
+
+            if (ageInput <= 18)
+            {
+                lblResult.Text = "You are a miner";
+            }
+        }
+    }
+}
